9d4573e99a407ac4926e5c3ef44b7a0ed677c510
Labib-Alwasi <55355136+Labib-Alwasi@users.noreply.github.com>
2025-12-23 14:29:55 +0000

    Add files via upload

---
+++ dates/date.hpp
@@ -0,0 +1,32 @@
+//
+// Created by HP on 29/10/2020.
+//
+
+#ifndef DATES3_DATE_HPP
+#define DATES3_DATE_HPP
+#include <iostream>
+using namespace std;
+
+#pragma once
+class date {
+private:
+    int year;
+    int month;
+    int day; // day of month
+    std::string weekday;
+
+public:
+    date(int y, int m, int d); // convenience constructor
+    date(); //default constructor
+    ~date();
+
+    void print_diagnostics(); // diagnostic only -- show us the values in the date object
+
+    // getters and setters should be here
+
+    // methods we are required to implement
+    int get_date_number();
+    int get_day_of_week();
+};
+
+#endif //DATES3_DATE_HPP
---
+++ dates/event.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#include "date.hpp"
+#pragma once
+
+class event :: public date {
+  private: 
+  void description;
+
+  public:
+  string get_event();
+}
